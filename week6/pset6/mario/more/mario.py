365c626b0ce7fee72c96601753bf89e84368f05a
chinhungtseng <bluesshown@gmail.com>
2021-03-09 14:10:37 +0800

    Add week6 pset: mario more version

---
+++ week6/pset6/mario/more/mario.py
@@ -0,0 +1,24 @@
+from cs50 import get_int
+
+
+def get_height(min=1, max=8):
+    """Prompt user for height value."""
+    while True:
+        height = get_int("Height: ")
+        if height >= min and height <= max:
+            return height
+
+
+def print_pyramid(n):
+    """Print n height of half-pyramid to console."""
+    for i in range(1, n+1):
+        print(" "*(n-i) + "#"*i + " "*2 + "#"*i)
+
+
+def main():
+    height = get_height()
+    print_pyramid(height)
+
+
+if __name__ == "__main__":
+    main()
