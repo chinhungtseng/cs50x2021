e1922bc61ef69761c65d8f76d82bbef6dd511724
chinhungtseng <bluesshown@gmail.com>
2021-03-24 20:42:21 +0800

    Revised week9 lab: birthday

--- week9/lab9/birthdays/application.py
+++ week9/lab9/birthdays/application.py
@@ -1,6 +1,5 @@
 import os
 
-from utils import month_day_valid
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 
@@ -16,27 +15,16 @@ db = SQL("sqlite:///birthdays.db")
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-
         # Add the user's entry into the database
-        if not (name := request.form.get("name")):
-            return render_template("error.html", message="Missing name")
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
 
-        if not (month := request.form.get("month")):
-            return render_template("error.html", message="Missing month")
-
-        if not (day := request.form.get("day")):
-            return render_template("error.html", message="Missing day")
-
-        if not month_day_valid(month, day):
-            return render_template("error.html", message="Invalid month or day, please check again")
-
-        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)",
-                   name, month, day)
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
 
         return redirect("/")
 
     else:
-
         # Display the entries in the database on index.html
         peoples = db.execute("SELECT * FROM birthdays;")
 
